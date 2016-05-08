194fae6cfce94daf5ac8ba3f668abcef87107b6c
dudu <nkdudu@126.com>
2016-05-08 17:58:00 +0800

    softmax

--- ex1/softmax_regression_vec.m
+++ ex1/softmax_regression_vec.m
@@ -10,6 +10,8 @@ function [f,g] = softmax_regression(theta, X,y)
   %       X(i,j) is the i'th coordinate of the j'th example.
   %   y - The label for each example.  y(j) is the j'th example's label.
   %
+  a = X;
+  b = y;
   m=size(X,2);
   n=size(X,1);
 
@@ -28,5 +30,39 @@ function [f,g] = softmax_regression(theta, X,y)
   %
 %%% YOUR CODE HERE %%%
   
+  %{
+  %	非常慢的循环做法  训练一次50分钟
+  theta=[theta, zeros(n,1)];
+  r = exp(theta' * X);   % (n*k)' * (n*m) = (k*m)
+  sumr = sum(r);         % 1*m
+  
+  for i = 1:m
+    f += -(([1:num_classes] == y(i)) * log(r(:,i)/sumr(i)));
+  end
+
+  for i = 1:m
+    p = r(:,i)/sumr(i);    % k*1
+    a = -(X(:,i) * (([1:num_classes] == y(i)) - p'));
+    g += a(:,1:end-1);
+  end
+  %}
+
+  %比较快的向量化做法  训练一次6分钟
+  theta=[theta, zeros(n,1)];
+  h = exp(theta' * X);  % k*m        
+  p = bsxfun(@rdivide,h,sum(h));  % k*m
+
+  c = log(p);          % k*m
+  i = sub2ind(size(c), y,1:size(c,2));  % y的值代表第i个样本的label k  从矩阵c的第i列中取出第k个值  返回的是编号 1*m
+  values = c(i);       % 1*m
+
+  f = -sum(values);
+
+
+  a = [1:num_classes]' == y;  % k*m
+  k = -X*(a - p)';
+  g = k(:,1:end-1);
+
+
   g=g(:); % make gradient a vector for minFunc
 
